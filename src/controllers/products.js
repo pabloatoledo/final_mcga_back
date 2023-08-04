21e36e31061cadff346196b545c5f5d1c2dbfc7d
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2023-08-04 18:17:52 -0300

    MCGA-BACK-012: find product by id

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -68,7 +68,6 @@ module.exports = {
     getStatus,
     getAll,
     getProductById,
-    getProductByName,
     remove,
     create,
     update    
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,7 +4,7 @@ const products = require('../controllers/products');
 router.get("/", products.getStatus);
 router.get("/all", products.getAll);
 router.get("/id/:productId", products.getProductById);
-router.get("/name/:productName", products.getProductByName);
+//router.get("/name/:productName", products.getProductByName);
 router.post("/",  products.create);
 router.delete("/:id", products.remove);
 router.put("/:id", products.update);
