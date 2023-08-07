aef0d5aae494ce37c44b430a73b917dbabf0037e
pabloatoledo <103152688+pabloatoledo@users.noreply.github.com>
2023-08-07 14:46:33 -0300

    MCGA-BACK-016: test cors options

--- index.js
+++ index.js
@@ -13,14 +13,13 @@ app.use(express.json());
 app.use(router)
 app.use(cors());
 
-const configureCors = (req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-};
-
-app.use(configureCors);
+});
 
 app.options('*', cors()); 
 
